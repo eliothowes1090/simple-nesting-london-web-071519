8fbec770d5e361b45ea9bfa16e3bd824ffecd31d
Eliot Howes <ejhowes@gmail.com>
2019-05-27 22:10:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -131,7 +131,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-binding.pry
 		 programmer_hash[:alan_kay][:known_for] = "GUI"
 		 return programmer_hash[:alan_kay][:known_for]
 		#  programmer_hash[:alan_kay][:languages]
