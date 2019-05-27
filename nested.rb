9be0dfc629e94085319b29b9710184a652190ed4
Eliot Howes <ejhowes@gmail.com>
2019-05-27 22:12:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,7 +111,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		#  return programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -131,7 +132,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:alan_kay][:known_for] = "GUI"
-		 return programmer_hash[:alan_kay][:known_for]
 		#  programmer_hash[:alan_kay][:languages]
 end
